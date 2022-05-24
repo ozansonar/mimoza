d2141963305a75ae270c3a56190da79599ea832f
mrokumus <okumusmr@gmail.com>
2022-05-24 10:20:42 +0300

    Page

--- admin/Controller/page.php
+++ admin/Controller/page.php
@@ -1,72 +1,73 @@
 <?php
-//sayfanın izin keyi
-$data->pageRoleKey = "page";
+
+use Mrt\MimozaCore\View;
+
+$pageRoleKey = "page";
 $pageAddRoleKey = "page-settings";
 
 //edit ve delete yapsa bile show (s) yetkisi olması lazım onu kontrol edelim
-if($session->sessionRoleControl($data->pageRoleKey,$constants::listPermissionKey) == false){
-    $log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"],"izinsiz erişim isteği user id->".$_SESSION["user_id"]." role key => ".$data->pageRoleKey." permissions => ".$constants::listPermissionKey);
-    $session->permissionDenied();
+if ($session->sessionRoleControl($pageRoleKey, $constants::listPermissionKey) === false) {
+	$log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"], "izinsiz erişim isteği user id->" . $_SESSION["user_id"] . " role key => " . $pageRoleKey . " permissions => " . $constants::listPermissionKey);
+	$session->permissionDenied();
 }
 //log atalım
 $log->logThis($log->logTypes['PAGE_LIST']);
 
 //bu sayfadakullanılan özel css'ler
-$customCss = [];
-$customCss[] = "plugins/datatables-bs4/css/dataTables.bootstrap4.min.css";
-$customCss[] = "plugins/datatables-responsive/css/responsive.bootstrap4.min.css";
-$customCss[] = "plugins/datatables-buttons/css/buttons.bootstrap4.min.css";
-//bu sayfadakullanılan özel js'ler
-$customJs = [];
-$customJs[] = "plugins/datatables/jquery.dataTables.min.js";
-$customJs[] = "plugins/datatables-bs4/js/dataTables.bootstrap4.min.js";
-$customJs[] = "plugins/datatables-responsive/js/dataTables.responsive.min.js";
-$customJs[] = "plugins/datatables-responsive/js/responsive.bootstrap4.min.js";
-$customJs[] = "plugins/datatables-buttons/js/dataTables.buttons.min.js";
-$customJs[] = "plugins/datatables-buttons/js/buttons.bootstrap4.min.js";
-$customJs[] = "plugins/jszip/jszip.min.js";
-$customJs[] = "plugins/pdfmake/pdfmake.min.js";
-$customJs[] = "plugins/pdfmake/vfs_fonts.js";
-$customJs[] = "plugins/datatables-buttons/js/buttons.html5.min.js";
-$customJs[] = "plugins/datatables-buttons/js/buttons.print.min.js";
-$customJs[] = "plugins/datatables-buttons/js/buttons.colVis.min.js";
+$customCss = [
+	"plugins/datatables-bs4/css/dataTables.bootstrap4.min.css",
+	"plugins/datatables-responsive/css/responsive.bootstrap4.min.css",
+	"plugins/datatables-buttons/css/buttons.bootstrap4.min.css",
+];
+$customJs = [
+	"plugins/datatables/jquery.dataTables.min.js",
+	"plugins/datatables-bs4/js/dataTables.bootstrap4.min.js",
+	"plugins/datatables-responsive/js/dataTables.responsive.min.js",
+	"plugins/datatables-responsive/js/responsive.bootstrap4.min.js",
+	"plugins/datatables-buttons/js/dataTables.buttons.min.js",
+	"plugins/datatables-buttons/js/buttons.bootstrap4.min.js",
+	"plugins/jszip/jszip.min.js",
+	"plugins/pdfmake/pdfmake.min.js",
+	"plugins/pdfmake/vfs_fonts.js",
+	"plugins/datatables-buttons/js/buttons.html5.min.js",
+	"plugins/datatables-buttons/js/buttons.print.min.js",
+	"plugins/datatables-buttons/js/buttons.colVis.min.js",
+];
 
-if(isset($_GET["delete"]) && !empty($_GET["delete"]) && is_numeric($_GET["delete"])){
-    //silme yetkisi kontrol
-    if($session->sessionRoleControl($data->pageRoleKey,$constants::deletePermissionKey) == false){
-        $log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"],"izinsiz erişim isteği user id->".$_SESSION["user_id"]." role key => ".$data->pageRoleKey." permissions => ".$constants::deletePermissionKey);
-        $session->permissionDenied();
-    }
+if (isset($_GET["delete"]) && !empty($_GET["delete"]) && is_numeric($_GET["delete"])) {
+	if ($session->sessionRoleControl($pageRoleKey, $constants::deletePermissionKey) === false) {
+		$log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"], "izinsiz erişim isteği user id->" . $_SESSION["user_id"] . " role key => " . $pageRoleKey . " permissions => " . $constants::deletePermissionKey);
+		$session->permissionDenied();
+	}
 
-    $del_id = $functions->cleanGetInt("delete");
-    $delete = $siteManager->multipleLanguageDataDelete("page",$del_id);
-    $message = [];
-    if($delete){
-        //log atalım
-        $log->logThis($log->logTypes['PAGE_LIST_DELETE_SUCC']);
-
-        $message["success"][] = $lang["content-delete"];
-        $refresh_time = 5;
-        $message["refresh_time"] = $refresh_time;
-        $functions->refresh($system->adminUrl("page"),$refresh_time);
-    }else{
-        //log atalım
-        $log->logThis($log->logTypes['PAGE_LIST_DELETE_ERR']);
-
-        $message["reply"][] = $lang["content-delete-error"] ;
-    }
+	$del_id = $functions->cleanGetInt("delete");
+	$delete = $siteManager->multipleLanguageDataDelete("page", $del_id);
+	$message = [];
+	if ($delete) {
+		$log->logThis($log->logTypes['PAGE_LIST_DELETE_SUCC']);
+		$message["success"][] = $lang["content-delete"];
+		$refresh_time = 5;
+		$message["refresh_time"] = $refresh_time;
+		$functions->refresh($system->adminUrl("page"), $refresh_time);
+	} else {
+		//log atalım
+		$log->logThis($log->logTypes['PAGE_LIST_DELETE_ERR']);
+		$message["reply"][] = $lang["content-delete-error"];
+	}
 }
-$data = $db::selectQuery("page",array(
-    "lang" => $siteManager->defaultLanguage()->short_lang,
-    "deleted" => 0,
+
+$data = $db::selectQuery("page", array(
+	"lang" => $siteManager->defaultLanguage()->short_lang,
+	"deleted" => 0,
 ));
 
-//sayfa başlıkları
-$page_title = "Sayfalar";
-$sub_title = "Bu sayfada ki içerikler diğer içeriklerden farklıdır burda olan içerikler siteadi.com/icerik-link olarak erişebilir \"İçerik işlemleri\" sayfasında olan içerikler ise siteadi.com/icerik-kategorisi/icerik-linki-icerik-id olarak gözükür.";
-//butonun gideceği link ve yazısı
-$data->pageButtonRedirectLink = "page-settings";
-$data->pageButtonRedirectText = "Yeni Ekle";
-$data->pageButtonIcon = "fas fa-plus-square";
 
-require $system->adminView('page');
+View::backend('page',[
+	'title' =>'Sayfalar',
+	'pageButtonRedirectLink' => "page-settings",
+	'pageButtonRedirectText' => "Yeni Ekle",
+	'pageButtonIcon' => "fas fa-plus-square",
+	'content' => $data,
+	'pageAddRoleKey'=>$pageAddRoleKey,
+	'pageRoleKey'=>$pageRoleKey
+]);
--- admin/View/project/page.php
+++ admin/View/project/page.php
@@ -1,128 +1,79 @@
-<?php
-/**
- *
- * Created by PhpStorm.
- * User: Ozan SONAR ( ozansonar1@gmail.com )
- * Date: 26.05.2020
- * Time: 15:47
- */
-?>
-<?php require $system->adminView('static/header'); ?>
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2 admin-page-top-settings">
-                    <div class="col-sm-6">
-                        <h1>
-                            <a href="javascript:goBack()"><i class="fas fa-arrow-circle-left"></i></a>
-                            <?php echo $data->title; ?>
-                        </h1>
-                    </div>
-                    <?php if($session->sessionRoleControl($data->pageRoleKey,$constants::listPermissionKey) == true): ?>
-                        <div class="col-sm-6 d-md-flex align-items-md-center justify-content-md-end">
-                            <h1>
-                                <a href="<?php echo $system->adminUrl($data->pageButtonRedirectLink); ?>">
-                                    <i class="<?php echo !empty($data->pageButtonIcon) ? $data->pageButtonIcon:"fas fa-th-list"; ?>"></i>
-                                    <?php echo $data->pageButtonRedirectText; ?>
-                                </a>
-                            </h1>
-                        </div>
-                    <?php endif; ?>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
-
-
-        <!-- Main content -->
-        <section class="content">
-
-            <?php if(!empty($sub_title)): ?>
-                <div class="alert alert-info alert-dismissible">
-                    <h5><i class="icon fas fa-info"></i> Dikkat !</h5>
-                    <?php echo $sub_title; ?>
-                </div>
-            <?php endif; ?>
-
-            <!-- Default box -->
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title"><?php echo $data->title; ?>
-                    </h3>
-
-                    <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                            <i class="fas fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-tool" data-card-widget="maximize">
-                            <i class="fas fa-expand"></i>
-                        </button>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <table id="datatable-1" class="table table-bordered table-striped">
-                        <thead>
-                        <tr>
-                            <th>Başlık</th>
-                            <th>Eklenme Tarihi</th>
-                            <th>Resim</th>
-                            <th>Durum</th>
-                            <th>İşlemler</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($data as $row): ?>
-                            <tr>
-                                <td>
-                                    <?php echo $functions->textModal($row->title,20); ?>
-                                </td>
-                                <td><?php echo $functions->dateShort($row->created_at); ?></td>
-                                <td>
-                                    <?php if(!empty($row->img) && file_exists($constants::fileTypePath["page_image"]["full_path"].$row->img)): ?>
-                                        <a href="<?php echo $constants::fileTypePath["page_image"]["url"].$row->img; ?>" data-toggle="lightbox" data-title="<?php echo $row->title; ?>" class="color-unset">
-                                            <i class="fas fa-images"></i>
-                                        </a>
-                                    <?php endif; ?>
-                                </td>
-                                <td><span class="<?php echo $constants::systemStatus[$row->status]["view_class"]; ?>"><?php echo $constants::systemStatus[$row->status]["view_text"]; ?></span></td>
-                                <td>
-                                    <?php if($session->sessionRoleControl($data->pageRoleKey,$constants::editPermissionKey) == true): ?>
-                                        <button type="button" class="btn btn-outline-success m-1" onclick="post_edit('<?php echo $system->adminUrl("page-settings?id=".$row->id); ?>')"><i class="fas fa-pencil-alt px-1"></i></i>Düzenle</button>
-                                    <?php endif; ?>
-                                    <?php if($session->sessionRoleControl($data->pageRoleKey,$constants::deletePermissionKey) == true): ?>
-                                        <button type="button" class="btn btn-outline-danger m-1" onclick="post_delete('<?php echo $system->adminUrl("page?delete=".$row->id); ?>')"><i class="fas fa-trash px-1"></i> Sil</button>
-                                    <?php endif; ?>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-                <!-- /.card-body -->
-                <div class="card-footer">
-
-                </div>
-                <!-- /.card-footer-->
+<section class="content">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">
+				<?php echo $data->title; ?>
+            </h3>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="maximize">
+                    <i class="fas fa-expand"></i>
+                </button>
             </div>
-            <!-- /.card -->
-
-        </section>
-        <!-- /.content -->
-
+        </div>
+        <div class="card-body">
+            <table id="datatable-1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>Başlık</th>
+                    <th>Eklenme Tarihi</th>
+                    <th>Resim</th>
+                    <th>Durum</th>
+                    <th>İşlemler</th>
+                </tr>
+                </thead>
+                <tbody>
+				<?php foreach ($data->content as $row): ?>
+                    <tr>
+                        <td>
+							<?php echo $functions->textModal($row->title, 20); ?>
+                        </td>
+                        <td><?php echo $functions->dateShort($row->created_at); ?></td>
+                        <td>
+							<?php if (!empty($row->img) && file_exists($constants::fileTypePath["page_image"]["full_path"] . $row->img)): ?>
+                                <a href="<?php echo $constants::fileTypePath["page_image"]["url"] . $row->img; ?>"
+                                   data-toggle="lightbox" data-title="<?php echo $row->title; ?>" class="color-unset">
+                                    <i class="fas fa-images"></i>
+                                </a>
+							<?php endif; ?>
+                        </td>
+                        <td>
+                            <span class="<?php echo $constants::systemStatus[$row->status]["view_class"]; ?>">
+                                <?php echo $constants::systemStatus[$row->status]["view_text"]; ?>
+                            </span>
+                        </td>
+                        <td>
+							<?php if ($session->sessionRoleControl($data->pageRoleKey, $constants::editPermissionKey) === true): ?>
+                                <button type="button" class="btn btn-outline-success m-1"
+                                        onclick="post_edit('<?php echo $system->adminUrl("page-settings?id=" . $row->id); ?>')">
+                                    <i class="fas fa-pencil-alt px-1"></i>
+                                    Düzenle
+                                </button>
+							<?php endif; ?>
+							<?php if ($session->sessionRoleControl($data->pageRoleKey, $constants::deletePermissionKey) === true): ?>
+                                <button type="button" class="btn btn-outline-danger m-1"
+                                        onclick="post_delete('<?php echo $system->adminUrl("page?delete=" . $row->id); ?>')">
+                                    <i class="fas fa-trash px-1"></i>
+                                    Sil
+                                </button>
+							<?php endif; ?>
+                        </td>
+                    </tr>
+				<?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-    <!-- /.content-wrapper -->
-    <script>
-        $(document).ready(function(){
-            $("#datatable-1").DataTable({
-                /*"language": {
-                    "url": "<?php echo $system->adminPublicUrl("plugins/datatables/lang/".$_SESSION["lang"].".json"); ?>"
-                },*/
-                "responsive": true,
-                "lengthChange": false,
-                "autoWidth": false,
-                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-            }).buttons().container().appendTo('#datatable-1_wrapper .col-md-6:eq(0)');
-        });
-    </script>
-<?php require $system->adminView('static/footer'); ?>
+</section>
+<script>
+    $(document).ready(function () {
+        $("#datatable-1").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        }).buttons().container().appendTo('#datatable-1_wrapper .col-md-6:eq(0)');
+    });
+</script>
