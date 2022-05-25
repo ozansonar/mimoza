65fc4d720d31708438d179609aa06a7c922ad5fa
mrokumus <okumusmr@gmail.com>
2022-05-25 11:30:53 +0300

    Email theme settings

--- admin/Controller/email-theme-settings.php
+++ admin/Controller/email-theme-settings.php
@@ -1,6 +1,8 @@
 <?php
 //sayfanın izin keyi
-$data->pageRoleKey = "email-themes";
+use Mrt\MimozaCore\View;
+
+$pageRoleKey = "email-themes";
 $pageAddRoleKey = "email-theme-settings";
 
 //sayfada işlem yapılacak table
@@ -8,165 +10,152 @@ $table = "email_template";
 
 $id = 0;
 $pageData = [];
-if(isset($_GET["id"])){
-    //update yetki kontrolü ve gösterme yetkisi de olması lazım
-    if($session->sessionRoleControl($data->pageRoleKey,$constants::editPermissionKey) == false || $session->sessionRoleControl($data->pageRoleKey,$constants::listPermissionKey) == false){
-        $log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"],"izinsiz erişim isteği user id->".$_SESSION["user_id"]." role key => ".$data->pageRoleKey." permissions => ".$constants::editPermissionKey);
-        $session->permissionDenied();
-    }
+if (isset($_GET["id"])) {
+	//update yetki kontrolü ve gösterme yetkisi de olması lazım
+	if ($session->sessionRoleControl($pageRoleKey, $constants::editPermissionKey) === false
+		|| $session->sessionRoleControl($pageRoleKey, $constants::listPermissionKey) === false) {
+		$log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"], "izinsiz erişim isteği user id->" . $_SESSION["user_id"] . " role key => " . $pageRoleKey . " permissions => " . $constants::editPermissionKey);
+		$session->permissionDenied();
+	}
 
-    //log atalım
-    $log->logThis($log->logTypes['EMAIL_TEMALARI_DETAIL']);
+	$log->logThis($log->logTypes['EMAIL_TEMALARI_DETAIL']);
 
-    //id ye ait içeriği çekiyoruz
-    $id = $functions->cleanGetInt("id");
-    $data = $db::selectQuery($table,array(
-        "id" => $id,
-        "deleted" => 0,
-    ),true);
-    if(empty($data)){
-        $functions->redirect($system->adminUrl());
-    }
-    //id ye ait içeriği çektik şimdi bulduğumuz datadan gelen lang_id ile eşleşen dataları bulup arraya atalım
-    $data_multi_lang = $db::selectQuery($table,array(
-        "lang_id" => $data->lang_id,
-        "deleted" => 0,
-    ));
-    foreach($data_multi_lang as $data_row){
-        $pageData[$data_row->lang] = (array) $data_row;
-        $db_data_lang[$data_row->lang] = $data_row->lang;
-    }
-}else{
-    //add yetki kontrolü
-    if($session->sessionRoleControl($pageAddRoleKey,$constants::addPermissionKey) == false){
-        $log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"],"izinsiz erişim isteği user id->".$_SESSION["user_id"]." role key => ".$data->pageRoleKey." permissions => ".$constants::editPermissionKey);
-        $session->permissionDenied();
-    }
-    //log atalım
-    $log->logThis($log->logTypes['EMAIL_TEMALARI_ADD_PAGE_DETAIL']);
+	$id = $functions->cleanGetInt("id");
+	$data = $db::selectQuery($table, array(
+		"id" => $id,
+		"deleted" => 0,
+	), true);
+
+	if (empty($data)) {
+		$functions->redirect($system->adminUrl());
+	}
+
+	//id ye ait içeriği çektik şimdi bulduğumuz datadan gelen lang_id ile eşleşen dataları bulup arraya atalım
+	$data_multi_lang = $db::selectQuery($table, array(
+		"lang_id" => $data->lang_id,
+		"deleted" => 0,
+	));
+
+	foreach ($data_multi_lang as $data_row) {
+		$pageData[$data_row->lang] = (array)$data_row;
+		$db_data_lang[$data_row->lang] = $data_row->lang;
+	}
+} else {
+	//add yetki kontrolü
+	if ($session->sessionRoleControl($pageAddRoleKey, $constants::addPermissionKey) === false) {
+		$log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"], "izinsiz erişim isteği user id->" . $_SESSION["user_id"] . " role key => " . $pageRoleKey . " permissions => " . $constants::editPermissionKey);
+		$session->permissionDenied();
+	}
+	//log atalım
+	$log->logThis($log->logTypes['EMAIL_TEMALARI_ADD_PAGE_DETAIL']);
 }
 
-//bu sayfadakullanılan özel css'ler
-$customCss = [];
-$customCss[] = "plugins/form-validation-engine/css/validationEngine.jquery.css";
-$customCss[] = "plugins/icheck-bootstrap/icheck-bootstrap.min.css";
-$customCss[] = "plugins/select2/css/select2.min.css";
-$customCss[] = "plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css";
-$customCss[] = "plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.css";
-$customCss[] = "plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.css";
-//bu sayfadakullanılan özel js'ler
-$customJs = [];
-$customJs[] = "plugins/form-validation-engine/js/jquery.validationEngine.js";
-$customJs[] = "plugins/form-validation-engine/js/languages/jquery.validationEngine-tr.js";
-$customJs[] = "plugins/select2/js/select2.full.min.js";
-$customJs[] = "plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.js";
-$customJs[] = "plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.js";
-$customJs[] = "plugins/ckeditor/ckeditor.js";
+$customCss = [
+	"plugins/form-validation-engine/css/validationEngine.jquery.css",
+	"plugins/icheck-bootstrap/icheck-bootstrap.min.css",
+	"plugins/select2/css/select2.min.css",
+	"plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css",
+	"plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.css",
+	"plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.css",
+];
+$customJs = [
+	"plugins/form-validation-engine/js/jquery.validationEngine.js",
+	"plugins/form-validation-engine/js/languages/jquery.validationEngine-tr.js",
+	"plugins/select2/js/select2.full.min.js",
+	"plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.js",
+	"plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.js",
+	"plugins/ckeditor/ckeditor.js",
+];
 
 
+if (isset($_POST["submit"]) && (int)$_POST["submit"] === 1) {
 
-if(isset($_POST["submit"]) && $_POST["submit"] == 1){
+	foreach ($projectLanguages as $project_languages_row) {
+		// formda dil ektensi olduğunu belirtiyoruz class ona göre post edecek
+		$functions->form_lang = $project_languages_row->short_lang;
+		$pageData[$project_languages_row->short_lang]["subject"] = $functions->cleanPost("subject");
+		$pageData[$project_languages_row->short_lang]["text"] = $functions->cleanPostTextarea("text");
+		$pageData[$project_languages_row->short_lang]["not_text"] = $functions->cleanPost("not_text");
+		$pageData[$project_languages_row->short_lang]["status"] = $functions->cleanPostInt("status");
 
-    foreach ($projectLanguages as $project_languages_row){
-        $functions->form_lang = $project_languages_row->short_lang; // formda dil ektensi olduğunu belirtiyoruz class ona göre post edecek
+		//istenilen kontroller
+		if ((int)$project_languages_row->form_validate === 1) {
+			if (empty($pageData[$project_languages_row->short_lang]["subject"])) {
+				$message["reply"][] = $project_languages_row->lang . " - Konu boş olamaz.";
+			}
 
-        $pageData[$project_languages_row->short_lang]["subject"] = $functions->cleanPost("subject");
-        $pageData[$project_languages_row->short_lang]["text"] = $functions->cleanPostTextarea("text");
-        $pageData[$project_languages_row->short_lang]["not_text"] = $functions->cleanPost("not_text");
-        $pageData[$project_languages_row->short_lang]["status"] = $functions->cleanPostInt("status");
+			if (!empty($pageData[$project_languages_row->short_lang]["subject"])) {
+				if (strlen($pageData[$project_languages_row->short_lang]["subject"]) < 2) {
+					$message["reply"][] = $project_languages_row->lang . " - Konu 2 karakterden az olamaz.";
+				}
+				if (strlen($pageData[$project_languages_row->short_lang]["subject"]) > 200) {
+					$message["reply"][] = $project_languages_row->lang . " - Konu 200 karakterden fazla olamaz.";
+				}
+			}
 
-        //istenilen kontroller
-        if($project_languages_row->form_validate == 1){
-            if(empty($pageData[$project_languages_row->short_lang]["subject"])){
-                $message["reply"][] = $project_languages_row->lang." - Konu boş olamaz.";
-            }
-
-            if(!empty($pageData[$project_languages_row->short_lang]["subject"])){
-                if(strlen($pageData[$project_languages_row->short_lang]["subject"]) < 2){
-                    $message["reply"][] = $project_languages_row->lang." - Konu 2 karakterden az olamaz.";
-                }
-                if(strlen($pageData[$project_languages_row->short_lang]["subject"]) > 200){
-                    $message["reply"][] = $project_languages_row->lang." - Konu 200 karakterden fazla olamaz.";
-                }
-            }
-
-            if(empty($pageData[$project_languages_row->short_lang]["text"])){
-                $message["reply"][] = $project_languages_row->lang." - E-posta içeriği boş olamaz.";
-            }
+			if (empty($pageData[$project_languages_row->short_lang]["text"])) {
+				$message["reply"][] = $project_languages_row->lang . " - E-posta içeriği boş olamaz.";
+			}
 
 
-            if(!in_array($pageData[$project_languages_row->short_lang]["status"],array_keys($constants::systemStatus))){
-                $message["reply"][] = $project_languages_row->lang." - Geçersiz onay durumu.";
-            }
-        }
-    }
-    if(empty($message)){
-        $lang_id = date("YmdHis");
-        foreach ($projectLanguages as $project_languages_row){
-            $db_data = [];
-            $db_data["subject"] = $pageData[$project_languages_row->short_lang]["subject"];
-            $db_data["text"] = $pageData[$project_languages_row->short_lang]["text"];
-            $db_data["not_text"] = $pageData[$project_languages_row->short_lang]["not_text"];
-            $db_data["status"] = $pageData[$project_languages_row->short_lang]["status"];
-            $db_data["user_id"] = $session->get("user_id");
-            if($id > 0){
-                if(array_key_exists($project_languages_row->short_lang,$db_data_lang)){
-                    //şuan ki dil db den gelen dataların içinde var bunu güncelle yoksa ekleyeceğiz
-                    //çünkü biz bu içeriği eklerken 1 dil olduğunu varsayalım 2. dili sisteme ekleyip bu içeriği update edersek 2.dili db ye insert etmesi lazım
-                    //güncelleme
-                    $update = $db::update($table,$db_data,array("id"=>$pageData[$project_languages_row->short_lang]["id"]));
-                }else{
-                    //yeni dil insert ediliyor
-                    //lang işlemleri sadece eklemede gönderilsin
-                    $db_data["lang"] = $project_languages_row->short_lang;
-                    $db_data["lang_id"] = $data->lang_id;
-                    $add = $db::insert($table,$db_data);
-                }
-            }else{
-                //ekleme
-                //lang işlemleri sadece eklemede gönderilsin
-                $db_data["lang"] = $project_languages_row->short_lang;
-                $db_data["lang_id"] = $lang_id;
-
-                $add = $db::insert($table,$db_data);
-            }
-        }
-        $refresh_time = 5;
-        $message["refresh_time"] = $refresh_time;
-        if($id > 0){
-            if($update){
-                //log atalım
-                $log->logThis($log->logTypes['EMAIL_TEMALARI_EDIT_SUCC']);
-                $message["success"][] = $lang["content-update"];
-
-                $functions->refresh($system->adminUrl($pageAddRoleKey."?id=".$id),$refresh_time);
-            }else{
-                //log atalım
-                $log->logThis($log->logTypes['EMAIL_TEMALARI_EDIT_ERR']);
-                $message["reply"][] = $lang["content-update-error"];
-            }
-        }else{
-            if($add){
-                //log atalım
-                $log->logThis($log->logTypes['EMAIL_TEMALARI_ADD_SUCC']);
-                $message["success"][] = $lang["content-insert"];
-
-                $functions->refresh($system->adminUrl($pageAddRoleKey),$refresh_time);
-            }else{
-                //log atalım
-                $log->logThis($log->logTypes['EMAIL_TEMALARI_ADD_ERR']);
-                $message["reply"][] = $lang["content-insert-error"];
-            }
-        }
-    }
+			if (!array_key_exists($pageData[$project_languages_row->short_lang]["status"], $constants::systemStatus)) {
+				$message["reply"][] = $project_languages_row->lang . " - Geçersiz onay durumu.";
+			}
+		}
+	}
+	if (empty($message)) {
+		$lang_id = date("YmdHis");
+		foreach ($projectLanguages as $project_languages_row) {
+			$db_data = [];
+			$db_data["subject"] = $pageData[$project_languages_row->short_lang]["subject"];
+			$db_data["text"] = $pageData[$project_languages_row->short_lang]["text"];
+			$db_data["not_text"] = $pageData[$project_languages_row->short_lang]["not_text"];
+			$db_data["status"] = $pageData[$project_languages_row->short_lang]["status"];
+			$db_data["user_id"] = $session->get("user_id");
+			if ($id > 0) {
+				if (array_key_exists($project_languages_row->short_lang, $db_data_lang)) {
+					$update = $db::update($table, $db_data, array("id" => $pageData[$project_languages_row->short_lang]["id"]));
+				} else {
+					$db_data["lang"] = $project_languages_row->short_lang;
+					$db_data["lang_id"] = $data->lang_id;
+					$add = $db::insert($table, $db_data);
+				}
+			} else {
+				$db_data["lang"] = $project_languages_row->short_lang;
+				$db_data["lang_id"] = $lang_id;
+				$add = $db::insert($table, $db_data);
+			}
+		}
+		$refresh_time = 3;
+		$message["refresh_time"] = $refresh_time;
+		if ($id > 0) {
+			if ($update) {
+				$log->logThis($log->logTypes['EMAIL_TEMALARI_EDIT_SUCC']);
+				$message["success"][] = $lang["content-update"];
+				$functions->refresh($system->adminUrl($pageAddRoleKey . "?id=" . $id), $refresh_time);
+			} else {
+				$log->logThis($log->logTypes['EMAIL_TEMALARI_EDIT_ERR']);
+				$message["reply"][] = $lang["content-update-error"];
+			}
+		} else if ($add) {
+			$log->logThis($log->logTypes['EMAIL_TEMALARI_ADD_SUCC']);
+			$message["success"][] = $lang["content-insert"];
+			$functions->refresh($system->adminUrl($pageAddRoleKey), $refresh_time);
+		} else {
+			$log->logThis($log->logTypes['EMAIL_TEMALARI_ADD_ERR']);
+			$message["reply"][] = $lang["content-insert-error"];
+		}
+	}
 }
 
+View::backend('email-theme-settings', [
+	'title' => "E-posta Teması " . (isset($data) ? "Düzenle" : "Ekle"),
+	'pageButtonRedirectLink' => $pageRoleKey,
+	'pageButtonRedirectText' => "E-posta Temaları",
+	'pageButtonIcon' => "icon-list",
+	'pageRoleKey' => $pageRoleKey,
+	'pageAddRoleKey' => $pageAddRoleKey,
+	'pageData' => $pageData,
+	'id' => $id,
 
-//sayfa başlıkları
-$page_title = "E-posta Teması ".(isset($data) ? "Düzenle":"Ekle");
-$sub_title = "E-posta içeriğinde <b>\"#\"</b> içinde yazılmış kelimleri lütfen silmeyin aksi halde e-posta içeriğinde yanlışlıklar olacaktır. Silmemeniz gereken örnek kelimeler <b>#ad_soyad#,#rezervasyon_kodu#</b> gibi kelimeleri silmeyiniz.";
-//butonun gideceği link ve yazısı
-$data->pageButtonRedirectLink = $data->pageRoleKey;
-$data->pageButtonRedirectText = "E-posta Temaları";
-$data->pageButtonIcon = "icon-list";
-require $system->adminView($pageAddRoleKey);
+]);
--- admin/View/project/email-theme-settings.php
+++ admin/View/project/email-theme-settings.php
@@ -1,34 +1,32 @@
 <section class="content">
-	<?php if (!empty($sub_title)): ?>
-        <div class="alert alert-info alert-dismissible">
-            <h5><i class="icon fas fa-info"></i> Dikkat !</h5>
-			<?php echo $sub_title; ?>
-        </div>
-	<?php endif; ?>
+
     <div class="card card-danger card-outline card-outline-tabs">
         <div class="card-header">
+            <div class="col-12 alert alert-info">
+                E-posta içeriğinde <b>"#"</b> içinde yazılmış kelimleri lütfen silmeyin aksi halde e-posta içeriğinde
+                yanlışlıklar olacaktır. Silmemeniz gereken örnek kelimeler <b>#ad_soyad#,#rezervasyon_kodu#</b> gibi
+                kelimeleri silmeyiniz.
+            </div>
             <ul class="nav nav-tabs" id="custom-tabs-two-tab" role="tablist">
                 <li class="pt-2 pr-3"><h3 class="card-title"><?php echo $data->title; ?></h3></li>
-				<?php foreach ($projectLanguages as $project_languages_row) {
-					?>
+				<?php foreach ($projectLanguages as $project_languages_row): ?>
                     <li class="nav-item">
                         <a class="nav-link <?php echo (int)$project_languages_row->default_lang === 1 ? "active" : null; ?>"
                            id="content-tab-<?php echo $project_languages_row->short_lang; ?>" data-toggle="pill"
                            href="#content-dashboard-<?php echo $project_languages_row->short_lang; ?>" role="tab"
                            aria-controls="content-dashboard-<?php echo $project_languages_row->short_lang; ?>"
-                           aria-selected="<?php echo (int)$project_languages_row->default_lang === 1 ? "true" : "false"; ?>"><?php echo $project_languages_row->lang; ?>
+                           aria-selected="<?php echo (int)$project_languages_row->default_lang === 1 ? "true" : "false"; ?>">
+							<?php echo $project_languages_row->lang; ?>
                             &nbsp;<i class="flag-icon flag-icon-<?php echo $project_languages_row->short_lang; ?>"></i></a>
                     </li>
-					<?php
-				} ?>
+				<?php endforeach ?>
             </ul>
         </div>
         <div class="card-body">
             <form action="" method="post" id="pageForm" enctype="multipart/form-data">
 				<?php echo $functions->csrfToken(); ?>
                 <div class="tab-content" id="custom-tabs-two-tabContent">
-					<?php foreach ($projectLanguages as $project_languages_row) {
-						?>
+					<?php foreach ($projectLanguages as $project_languages_row): ?>
                         <div class="tab-pane fade <?php echo (int)$project_languages_row->default_lang === 1 ? "show active" : null; ?>"
                              id="content-dashboard-<?php echo $project_languages_row->short_lang; ?>" role="tabpanel"
                              aria-labelledby="content-tab-<?php echo $project_languages_row->short_lang; ?>">
@@ -52,20 +50,16 @@
 							), $data->pageData);
 							?>
                         </div>
-						<?php
-					} ?>
+					<?php endforeach ?>
                 </div>
             </form>
         </div>
-        <!-- /.card-body -->
         <div class="card-footer text-right">
-			<?php
-			echo $form->button("submit", array(
+			<?php echo $form->button("submit", array(
 				"text" => "Kaydet",
 				"icon" => "fas fa-save",
 				"btn_class" => "btn btn-success",
-			));
-			?>
+			)); ?>
         </div>
     </div>
 </section>
