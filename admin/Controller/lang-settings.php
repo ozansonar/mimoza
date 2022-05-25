984937029049ba3091dbb3ce1b3825c286d816d6
mrokumus <okumusmr@gmail.com>
2022-05-25 10:53:54 +0300

    Lang settings

--- admin/Controller/lang-settings.php
+++ admin/Controller/lang-settings.php
@@ -1,177 +1,168 @@
 <?php
 //sayfanın izin keyi
-$data->pageRoleKey = "lang";
+use Mrt\MimozaCore\View;
+
+$pageRoleKey = "lang";
 $pageAddRoleKey = "lang-settings";
 
 $id = 0;
 $pageData = [];
 
-$default_lang = $siteManager->defaultLanguage();
+$defaultLanguage = $siteManager->defaultLanguage();
 
-if(isset($_GET["id"])){
-    //update yetki kontrolü ve gösterme yetkisi de olması lazım
-    if($session->sessionRoleControl($data->pageRoleKey,$constants::editPermissionKey) == false || $session->sessionRoleControl($data->pageRoleKey,$constants::listPermissionKey) == false){
-        $log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"],"izinsiz erişim isteği user id->".$_SESSION["user_id"]." role key => ".$data->pageRoleKey." permissions => ".$constants::editPermissionKey);
-        $session->permissionDenied();
-    }
+if (isset($_GET["id"])) {
+	//update yetki kontrolü ve gösterme yetkisi de olması lazım
+	if ($session->sessionRoleControl($pageRoleKey, $constants::editPermissionKey) === false
+		|| $session->sessionRoleControl($pageRoleKey, $constants::listPermissionKey) === false) {
+		$log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"], "izinsiz erişim isteği user id->" . $_SESSION["user_id"] . " role key => " . $pageRoleKey . " permissions => " . $constants::editPermissionKey);
+		$session->permissionDenied();
+	}
 
-    //log atalım
-    $log->logThis($log->logTypes['LANG_DETAIL']);
+	$log->logThis($log->logTypes['LANG_DETAIL']);
+	$id = $functions->cleanGetInt("id");
+	$data = $db::selectQuery("lang", array(
+		"id" => $id,
+		"deleted" => 0,
+	), true);
 
-    //id ye ait içeriği çekiyoruz
-    $id = $functions->cleanGetInt("id");
-    $data = $db::selectQuery("lang",array(
-        "id" => $id,
-        "deleted" => 0,
-    ),true);
-    if(empty($data)){
-        $functions->redirect($system->adminUrl());
-    }
-    $pageData[$default_lang->short_lang] = (array) $data;
-}else{
-    //add yetki kontrolü
-    if($session->sessionRoleControl($pageAddRoleKey,$constants::addPermissionKey) == false){
-        $log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"],"izinsiz erişim isteği user id->".$_SESSION["user_id"]." role key => ".$data->pageRoleKey." permissions => ".$constants::editPermissionKey);
-        $session->permissionDenied();
-    }
-    //log atalım
-    $log->logThis($log->logTypes['LANG_ADD_PAGE']);
+	if (empty($data)) {
+		$functions->redirect($system->adminUrl());
+	}
+	$pageData[$defaultLanguage->short_lang] = (array)$data;
+} else {
+	if ($session->sessionRoleControl($pageAddRoleKey, $constants::addPermissionKey) === false) {
+		$log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"], "izinsiz erişim isteği user id->" . $_SESSION["user_id"] . " role key => " . $pageRoleKey . " permissions => " . $constants::editPermissionKey);
+		$session->permissionDenied();
+	}
+	$log->logThis($log->logTypes['LANG_ADD_PAGE']);
 }
 
-//bu sayfadakullanılan özel css'ler
-$customCss = [];
-$customCss[] = "plugins/form-validation-engine/css/validationEngine.jquery.css";
-$customCss[] = "plugins/icheck-bootstrap/icheck-bootstrap.min.css";
-$customCss[] = "plugins/select2/css/select2.min.css";
-$customCss[] = "plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css";
-$customCss[] = "plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.css";
-$customCss[] = "plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.css";
-//bu sayfadakullanılan özel js'ler
-$customJs = [];
-$customJs[] = "plugins/form-validation-engine/js/jquery.validationEngine.js";
-$customJs[] = "plugins/form-validation-engine/js/languages/jquery.validationEngine-tr.js";
-$customJs[] = "plugins/select2/js/select2.full.min.js";
-$customJs[] = "plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.js";
-$customJs[] = "plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.js";
-$customJs[] = "plugins/ckeditor/ckeditor.js";
+$customCss = [
+	"plugins/form-validation-engine/css/validationEngine.jquery.css",
+	"plugins/icheck-bootstrap/icheck-bootstrap.min.css",
+	"plugins/select2/css/select2.min.css",
+	"plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css",
+	"plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.css",
+	"plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.css",
+];
+$customJs = [
+	"plugins/form-validation-engine/js/jquery.validationEngine.js",
+	"plugins/form-validation-engine/js/languages/jquery.validationEngine-tr.js",
+	"plugins/select2/js/select2.full.min.js",
+	"plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.js",
+	"plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.js",
+	"plugins/ckeditor/ckeditor.js",
+];
 
 
-if(isset($_POST["submit"]) && $_POST["submit"] == 1){
+if (isset($_POST["submit"]) && (int)$_POST["submit"] === 1) {
 
-    $functions->form_lang = $default_lang->short_lang;
-    $pageData[$default_lang->short_lang]["lang"] = $functions->cleanPost("lang");
-    $pageData[$default_lang->short_lang]["short_lang"] = $functions->cleanPost("short_lang");
-    $pageData[$default_lang->short_lang]["default_lang"] = $functions->cleanPostInt("default_lang");
-    $pageData[$default_lang->short_lang]["form_validate"] = $functions->cleanPostInt("form_validate");
-    $pageData[$default_lang->short_lang]["status"] = $functions->cleanPostInt("status");
-
-
-    if(empty($pageData[$default_lang->short_lang]["lang"])){
-        $message["reply"][] = "Dil boş olamaz.";
-    }
-
-    if(!empty($pageData[$default_lang->short_lang]["name"])){
-        if(strlen($pageData[$default_lang->short_lang]["name"]) < 2){
-            $message["reply"][] = "Dil 2 karakterden az olamaz.";
-        }
-        if(strlen($pageData[$default_lang->short_lang]["name"]) > 20){
-            $message["success"][] = "Dil 20 karakterden fazla olamaz.";
-        }
-    }
-
-    if(empty($pageData[$default_lang->short_lang]["short_lang"])){
-        $message["reply"][] = "Dil kısaltma boş olamaz.";
-    }
-
-    if(!empty($pageData[$default_lang->short_lang]["short_lang"])){
-        if(strlen($pageData[$default_lang->short_lang]["short_lang"]) < 2){
-            $message["reply"][] = "Dil kısaltma 2 karakterden az olamaz.";
-        }
-        if(strlen($pageData[$default_lang->short_lang]["short_lang"]) > 5){
-            $message["success"][] = "Dil kısaltma 5 karakterden fazla olamaz.";
-        }
-    }
-
-    if(!in_array($pageData[$default_lang->short_lang]["default_lang"],array_keys($constants::systemSiteModVersion2))){
-        $message["reply"][] = "Geçersiz varsayılan dil durumu.";
-    }
-
-    if(!in_array($pageData[$default_lang->short_lang]["form_validate"],array_keys($constants::systemSiteModVersion2))){
-        $message["reply"][] = "Geçersiz form doğrulama durumu.";
-    }
-
-    if(!in_array($pageData[$default_lang->short_lang]["status"],array_keys($constants::systemStatus))){
-        $message["reply"][] = "Geçersiz onay durumu.";
-    }
-
-    if($pageData[$default_lang->short_lang]["default_lang"] == 1 && $pageData[$default_lang->short_lang]["form_validate"] != 1){
-        $message["reply"][] = "Bu dili varsayılan dil olarak seçtiniz bu durumda <b>Form Doğrulama</b> seçeneğinide evet olarak seçmeniz gerekmektedir.";
-    }
-    //varsayılan dil başka var mı ?
-    if($pageData[$default_lang->short_lang]["default_lang"] != 1 && $siteManager->getDefaultLangNotId($id) == false){
-        $message["reply"][] = "Varsayılan başka bir dil yok bu dili varsayılan dil yapmak zorundasınız. Bu dili varsayılandan çıkarmak istiyorsanız başka bir dili varsayılan yapmanız yeterli olacaktır.";
-    }
+	$functions->form_lang = $defaultLanguage->short_lang;
+	$pageData[$defaultLanguage->short_lang]["lang"] = $functions->cleanPost("lang");
+	$pageData[$defaultLanguage->short_lang]["short_lang"] = $functions->cleanPost("short_lang");
+	$pageData[$defaultLanguage->short_lang]["default_lang"] = $functions->cleanPostInt("default_lang");
+	$pageData[$defaultLanguage->short_lang]["form_validate"] = $functions->cleanPostInt("form_validate");
+	$pageData[$defaultLanguage->short_lang]["status"] = $functions->cleanPostInt("status");
 
 
-    if(empty($message)){
-        $db_data = [];
-        $db_data["lang"] = $pageData[$default_lang->short_lang]["lang"];
-        $db_data["short_lang"] = $pageData[$default_lang->short_lang]["short_lang"];
-        $db_data["default_lang"] = $pageData[$default_lang->short_lang]["default_lang"];
-        $db_data["form_validate"] = $pageData[$default_lang->short_lang]["form_validate"];
-        $db_data["status"] = $pageData[$default_lang->short_lang]["status"];
-        $db_data["user_id"] = $session->get("user_id");
+	if (empty($pageData[$defaultLanguage->short_lang]["lang"])) {
+		$message["reply"][] = "Dil boş olamaz.";
+	}
 
-        $refresh_time = 5;
-        $message["refresh_time"] = $refresh_time;
-        if($id > 0){
-            $update = $db::update("lang",$db_data,array("id"=>$id));
-            if($update){
-                //log atalım
-                $log->logThis($log->logTypes['LANG_EDIT_SUCC']);
-                $message["success"][] = $lang["content-update"];
+	if (!empty($pageData[$defaultLanguage->short_lang]["name"])) {
+		if (strlen($pageData[$defaultLanguage->short_lang]["name"]) < 2) {
+			$message["reply"][] = "Dil 2 karakterden az olamaz.";
+		}
+		if (strlen($pageData[$defaultLanguage->short_lang]["name"]) > 20) {
+			$message["success"][] = "Dil 20 karakterden fazla olamaz.";
+		}
+	}
 
-                if($pageData[$default_lang->short_lang]["default_lang"] == 1){
-                    //işlem tamamlandı eğer varsayılan dil yapıldıysa diğer varsayılan dili kaldıralım
-                    $siteManager->defaultLanguageReset($id);
-                }
+	if (empty($pageData[$defaultLanguage->short_lang]["short_lang"])) {
+		$message["reply"][] = "Dil kısaltma boş olamaz.";
+	}
 
-                $functions->refresh($system->adminUrl("lang-settings?id=".$id),$refresh_time);
-            }else{
-                //log atalım
-                $log->logThis($log->logTypes['LANG_EDIT_ERR']);
-                $message["reply"][] = $lang["content-update-error"];
-            }
-        }else{
-            //ekleme
-            $add = $db::insert("lang",$db_data);
+	if (!empty($pageData[$defaultLanguage->short_lang]["short_lang"])) {
+		if (strlen($pageData[$defaultLanguage->short_lang]["short_lang"]) < 2) {
+			$message["reply"][] = "Dil kısaltma 2 karakterden az olamaz.";
+		}
+		if (strlen($pageData[$defaultLanguage->short_lang]["short_lang"]) > 5) {
+			$message["success"][] = "Dil kısaltma 5 karakterden fazla olamaz.";
+		}
+	}
 
-            if($add){
-                //log atalım
-                $log->logThis($log->logTypes['LANG_ADD_SUCC']);
-                $message["success"][] = $lang["content-insert"];
+	if (!array_key_exists($pageData[$defaultLanguage->short_lang]["default_lang"], $constants::systemSiteModVersion2)) {
+		$message["reply"][] = "Geçersiz varsayılan dil durumu.";
+	}
 
-                if($pageData[$default_lang->short_lang]["default_lang"] == 1){
-                    //işlem tamamlandı eğer varsayılan dil yapıldıysa diğer varsayılan dili kaldıralım
-                    $siteManager->defaultLanguageReset($db->getLastInsertedId());
-                }
+	if (!array_key_exists($pageData[$defaultLanguage->short_lang]["form_validate"], $constants::systemSiteModVersion2)) {
+		$message["reply"][] = "Geçersiz form doğrulama durumu.";
+	}
 
-                $functions->refresh($system->adminUrl("lang-settings"),$refresh_time);
-            }else{
-                //log atalım
-                $log->logThis($log->logTypes['LANG_ADD_ERR']);
-                $message["reply"][] = $lang["content-insert-error"];
-            }
-        }
-    }
+	if (!array_key_exists($pageData[$defaultLanguage->short_lang]["status"], $constants::systemStatus)) {
+		$message["reply"][] = "Geçersiz onay durumu.";
+	}
+
+	if ((int)$pageData[$defaultLanguage->short_lang]["default_lang"] === 1 && (int)$pageData[$defaultLanguage->short_lang]["form_validate"] !== 1) {
+		$message["reply"][] = "Bu dili varsayılan dil olarak seçtiniz bu durumda <b>Form Doğrulama</b> seçeneğinide evet olarak seçmeniz gerekmektedir.";
+	}
+
+	if ((int)$pageData[$defaultLanguage->short_lang]["default_lang"] !== 1 && $siteManager->getDefaultLangNotId($id) === false) {
+		$message["reply"][] = "Varsayılan başka bir dil yok bu dili varsayılan dil yapmak zorundasınız. Bu dili varsayılandan çıkarmak istiyorsanız başka bir dili varsayılan yapmanız yeterli olacaktır.";
+	}
+
+
+	if (empty($message)) {
+		$db_data = [];
+		$db_data["lang"] = $pageData[$defaultLanguage->short_lang]["lang"];
+		$db_data["short_lang"] = $pageData[$defaultLanguage->short_lang]["short_lang"];
+		$db_data["default_lang"] = $pageData[$defaultLanguage->short_lang]["default_lang"];
+		$db_data["form_validate"] = $pageData[$defaultLanguage->short_lang]["form_validate"];
+		$db_data["status"] = $pageData[$defaultLanguage->short_lang]["status"];
+		$db_data["user_id"] = $session->get("user_id");
+
+		$refresh_time = 3;
+		$message["refresh_time"] = $refresh_time;
+		if ($id > 0) {
+			$update = $db::update("lang", $db_data, array("id" => $id));
+			if ($update) {
+				$log->logThis($log->logTypes['LANG_EDIT_SUCC']);
+				$message["success"][] = $lang["content-update"];
+				if ((int)$pageData[$defaultLanguage->short_lang]["default_lang"] === 1) {
+					$siteManager->defaultLanguageReset($id);
+				}
+				$functions->refresh($system->adminUrl("lang-settings?id=" . $id), $refresh_time);
+			} else {
+				$log->logThis($log->logTypes['LANG_EDIT_ERR']);
+				$message["reply"][] = $lang["content-update-error"];
+			}
+		} else {
+			$add = $db::insert("lang", $db_data);
+			if ($add) {
+				$log->logThis($log->logTypes['LANG_ADD_SUCC']);
+				$message["success"][] = $lang["content-insert"];
+
+				if ((int)$pageData[$defaultLanguage->short_lang]["default_lang"] === 1) {
+					//işlem tamamlandı eğer varsayılan dil yapıldıysa diğer varsayılan dili kaldıralım
+					$siteManager->defaultLanguageReset($db::getLastInsertedId());
+				}
+				$functions->refresh($system->adminUrl("lang-settings"), $refresh_time);
+			} else {
+				//log atalım
+				$log->logThis($log->logTypes['LANG_ADD_ERR']);
+				$message["reply"][] = $lang["content-insert-error"];
+			}
+		}
+	}
 }
 
-
-//sayfa başlıkları
-$page_title = "Dil ".(isset($data) ? "Düzenle":"Ekle");
-$sub_title = null;
-//butonun gideceği link ve yazısı
-$data->pageButtonRedirectLink = "lang";
-$data->pageButtonRedirectText = "Diller";
-$data->pageButtonIcon = "icon-list";
-require $system->adminView('lang-settings');
+View::backend('lang-settings', [
+	'title' => "Dil " . (isset($data) ? "Düzenle" : "Ekle"),
+	'pageButtonRedirectLink' => "lang",
+	'pageButtonRedirectText' => "Diller",
+	'pageButtonIcon' => "icon-list",
+	'pageRoleKey' => $pageRoleKey,
+	'pageAddRoleKey' => $pageAddRoleKey,
+	'defaultLanguage' => $defaultLanguage,
+	'pageData' => $pageData,
+]);
--- admin/View/project/lang-settings.php
+++ admin/View/project/lang-settings.php
@@ -1,18 +1,9 @@
 <section class="content">
-
-	<?php if (!empty($sub_title)): ?>
-        <div class="alert alert-info alert-dismissible">
-            <h5><i class="icon fas fa-info"></i> Dikkat !</h5>
-			<?php echo $sub_title; ?>
-        </div>
-	<?php endif; ?>
-
-    <!-- Default box -->
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title"><?php echo $data->title; ?>
+            <h3 class="card-title">
+                <?php echo $data->title; ?>
             </h3>
-
             <div class="card-tools">
                 <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                     <i class="fas fa-minus"></i>
@@ -25,7 +16,7 @@
         <div class="card-body">
             <form action="" method="post" id="pageForm" enctype="multipart/form-data">
 				<?php
-				$form->lang = $default_lang->short_lang;
+				$form->lang = $data->defaultLanguage->short_lang;
 				$form->form_name_no_lang = 1;
 				echo $functions->csrfToken();
 				echo $form->input("lang", array(
@@ -54,20 +45,14 @@
 				?>
             </form>
         </div>
-        <!-- /.card-body -->
         <div class="card-footer text-right">
-			<?php
-			echo $form->button("submit", array(
+			<?php echo $form->button("submit", array(
 				"text" => "Kaydet",
 				"icon" => "fas fa-save",
 				"btn_class" => "btn btn-success",
-			));
-			?>
+			)); ?>
         </div>
-        <!-- /.card-footer-->
     </div>
-    <!-- /.card -->
-
 </section>
 <script>
     $(document).ready(function () {
