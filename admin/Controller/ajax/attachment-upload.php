195994747a6aa9617a27ede0092bb2c7738e7bfe
mrokumus <okumusmr@gmail.com>
2022-05-26 11:42:25 +0300

    Update attachment-upload.php
    Yeni sistem düzeltmeleri

--- admin/Controller/ajax/attachment-upload.php
+++ admin/Controller/ajax/attachment-upload.php
@@ -1,22 +1,16 @@
 <?php
-/**
- * Author: Ozan SONAR
- * Mail : ozansonar1@gmail.com
- * User: Ozan PC
- * Date: 11.03.2021
- * Time: 15:54
- */
+
+use Mrt\MimozaCore\FileUploader;
 
 $message = [];
 if(isset($_FILES["attachment"]) && !empty($_FILES["attachment"]["name"])){
-    include_once($system->path("includes/System/FileUploader.php"));
-    $file = new \Includes\System\FileUploader($constants::fileTypePath);
+    $file = new FileUploader($constants::fileTypePath);
     $file->globalFileName = "attachment";
     $file->uploadFolder = "mailing_attachment";
-    $file->upload_type = "pdf_word_image_excel";
+    $file->uploadType = "pdf_word_image_excel";
     $file->maxFileSize = 5;
     $uploaded = $file->fileUpload();
-    if($uploaded["result"] == 1){
+    if((int)$uploaded["result"] === 1){
         $message["success"][] = "Ek başarıyla yüklendi.";
         $message["img_path"] = $constants::fileTypePath["mailing_attachment"]["url"].$uploaded["img_name"];
         $message["img_name"] = $uploaded["img_name"];
