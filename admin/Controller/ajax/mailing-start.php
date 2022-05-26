70d3c6019b1f38b7368cb39a938a99b988c0e2c1
mrokumus <okumusmr@gmail.com>
2022-05-26 11:54:35 +0300

    Update mailing-start.php

--- admin/Controller/ajax/mailing-start.php
+++ admin/Controller/ajax/mailing-start.php
@@ -5,8 +5,6 @@ use Mrt\MimozaCore\Mail;
 $message = [];
 if (isset($_POST["mailing_id"])) {
 	$id = $functions->cleanPostInt("mailing_id");
-
-	include_once $system->path("includes/System/Mail.php");
 	$mail_class = new Mail($db);
 	$send = $mail_class->sendMailing($id);
 	$message = $send;
