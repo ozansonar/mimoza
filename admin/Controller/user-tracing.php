d7fdb108c98abaf6441022bea105edd03bd77291
mrokumus <okumusmr@gmail.com>
2022-05-26 10:55:08 +0300

    User Tracing
    Kullanıcı bazlı loglama sayfası düzenlendi

--- admin/Controller/user-tracing.php
+++ admin/Controller/user-tracing.php
@@ -1,43 +1,49 @@
 <?php
 
-$data->pageRoleKey = "user-tracing";
-if ($session->sessionRoleControl($data->pageRoleKey, $constants::listPermissionKey) == false){
-    $log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"],"izinsiz erişim isteği user id->".$_SESSION["user_id"]." role key => ".$data->pageRoleKey." permissions => ".$constants::editPermissionKey);
-    $session->permissionDenied();
+use Mrt\MimozaCore\View;
+
+$pageRoleKey = "user-tracing";
+if ($session->sessionRoleControl($pageRoleKey, $constants::listPermissionKey) === false) {
+	$log->logThis($log->logTypes["IZINSIZ_ERISIM_ISTEGI"], "izinsiz erişim isteği user id->" . $_SESSION["user_id"] . " role key => " . $pageRoleKey . " permissions => " . $constants::editPermissionKey);
+	$session->permissionDenied();
 }
 
-//log atalım
 $log->logThis($log->logTypes['USER_TRACING']);
 
 $id = 0;
-if(isset($_GET["id"])){
-    $id = $functions->cleanGetInt("id");
-    $selectQuery = $db::selectQuery("users",array(
-        "id" => $id,
-        "deleted" => 0,
-    ),true);
-    if(empty($selectQuery)){
-        $functions->redirect($system->adminUrl());
-    }
-    $logs_get = $db::query("SELECT l.*,lt.log_key FROM logs l INNER JOIN log_types lt ON lt.log_val=l.log_type WHERE user_id=:uid ORDER BY l.id DESC");
-    $logs_get->bindParam(":uid",$id,PDO::PARAM_INT);
-    $logs_get->execute();
-    $logs_count = $logs_get->rowCount();
-    $log_array = [];
-    if($logs_count > 0){
-        $logs_data = $logs_get->fetchAll(PDO::FETCH_OBJ);
-        foreach ($logs_data as $logs_row){
-            $date = date('Y-m-d', strtotime($logs_row->log_datetime));
-            $log_array[$date][] = $logs_row;
-        }
-    }
+if (isset($_GET["id"])) {
+	$id = $functions->cleanGetInt("id");
+	$selectQuery = $db::selectQuery("users", array(
+		"id" => $id,
+		"deleted" => 0,
+	), true);
+	if (empty($selectQuery)) {
+		$functions->redirect($system->adminUrl());
+	}
+	$logs_get = $db::query("SELECT l.*,lt.log_key FROM logs l INNER JOIN log_types lt ON lt.log_val=l.log_type WHERE user_id=:uid ORDER BY l.id DESC");
+	$logs_get->bindParam(":uid", $id, PDO::PARAM_INT);
+	$logs_get->execute();
+	$logs_count = $logs_get->rowCount();
+	$log_array = [];
+	if ($logs_count > 0) {
+		$logs_data = $logs_get->fetchAll(PDO::FETCH_OBJ);
+		foreach ($logs_data as $logs_row) {
+			$date = date('Y-m-d', strtotime($logs_row->log_datetime));
+			$log_array[$date][] = $logs_row;
+		}
+	}
+}
+if ($id === 0) {
+	$functions->redirect('user');
 }
 
-//sayfa başlıkları
-$page_title = "Kullanıcı Hareketleri";
-$sub_title = $selectQuery->name." ".$selectQuery->surname." isimli kullanıcının sistemimizdeki hareketleri.";
-//butonun gideceği link ve yazısı
-$data->pageButtonRedirectLink = "user";
-$data->pageButtonRedirectText = "Kullancılar";
-$data->pageButtonIcon = "icon-list";
-require $system->adminView('user-tracing');
+View::backend('user-tracing', [
+	'title' => "Kullanıcı Hareketleri",
+	'subTitle' => $selectQuery->name . " " . $selectQuery->surname . " isimli kullanıcının sistemimizdeki hareketleri.",
+	'id' => $id,
+	'pageButtonRedirectLink' => "user",
+	'pageButtonRedirectText' => "Kullancılar",
+	'pageButtonIcon' => "icon-list",
+	'pageRoleKey' => $pageRoleKey,
+	'logs' => $log_array,
+]);
--- admin/View/project/user-tracing.php
+++ admin/View/project/user-tracing.php
@@ -1,136 +1,88 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ozan PC
- * Date: 2.09.2020
- * Time: 20:56
- * Email: ozansonar1@gmail.com
- */
-?>
-<?php require $system->adminView('static/header'); ?>
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2 admin-page-top-settings">
-                <div class="col-sm-6">
-                    <h1>
-                        <a href="javascript:goBack()"><i class="fas fa-arrow-circle-left"></i></a>
-                        <?php echo $data->title; ?>
-                    </h1>
-                </div>
-                <?php if($session->sessionRoleControl($data->pageRoleKey,$constants::listPermissionKey) == true): ?>
-                    <div class="col-sm-6 d-md-flex align-items-md-center justify-content-md-end">
-                        <h1>
-                            <a href="<?php echo $system->adminUrl($data->pageButtonRedirectLink); ?>">
-                                <i class="<?php echo !empty($data->pageButtonIcon) ? $data->pageButtonIcon:"fas fa-th-list"; ?>"></i>
-                                <?php echo $data->pageButtonRedirectText; ?>
-                            </a>
-                        </h1>
-                    </div>
-                <?php endif; ?>
+<section class="content">
+	<?php if (!empty($data->subTitle)): ?>
+        <div class="alert alert-info alert-dismissible">
+            <h5><i class="icon fas fa-info"></i> Dikkat !</h5>
+			<?php echo $data->subTitle; ?>
+        </div>
+	<?php endif; ?>
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">
+				<?php echo $data->title; ?>
+            </h3>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="maximize">
+                    <i class="fas fa-expand"></i>
+                </button>
             </div>
-        </div><!-- /.container-fluid -->
-    </section>
-
-
-    <!-- Main content -->
-    <section class="content">
-
-        <?php if(!empty($sub_title)): ?>
-            <div class="alert alert-info alert-dismissible">
-                <h5><i class="icon fas fa-info"></i> Dikkat !</h5>
-                <?php echo $sub_title; ?>
-            </div>
-        <?php endif; ?>
-
-        <!-- Default box -->
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title"><?php echo $data->title; ?>
-                </h3>
-
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                        <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="maximize">
-                        <i class="fas fa-expand"></i>
-                    </button>
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <?php if(!empty($log_array)): ?>
-                        <div class="col-5 col-sm-3">
-                            <div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist" aria-orientation="vertical">
-                                <?php $log_count = 1; ?>
-                                <?php foreach ($log_array as $log_key=>$log): ?>
-                                    <a class="nav-link <?php echo $log_count == 1 ? "active":null; ?>" id="<?php echo $log_key; ?>-tab" data-toggle="pill" href="#tab-<?php echo $log_key; ?>" role="tab" aria-controls="tab-<?php echo $log_key; ?>" aria-selected="<?php echo $log_count == 1 ? "true":"false"; ?>"><?php echo $functions->dateLong($log_key); ?></a>
-                                    <?php $log_count++; ?>
-                                <?php endforeach; ?>
-                            </div>
+        </div>
+        <div class="card-body">
+            <div class="row">
+				<?php if (!empty($data->logs)): ?>
+                    <div class="col-5 col-sm-3">
+                        <div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist"
+                             aria-orientation="vertical">
+							<?php $count = 1; ?>
+							<?php foreach ($data->logs as $log_key => $log): ?>
+                                <a class="nav-link <?php echo $count === 1 ? "active" : null; ?>"
+                                   id="<?php echo $log_key; ?>-tab" data-toggle="pill"
+                                   href="#tab-<?php echo $log_key; ?>" role="tab"
+                                   aria-controls="tab-<?php echo $log_key; ?>"
+                                   aria-selected="<?php echo $count === 1 ? "true" : "false"; ?>">
+									<?php echo $functions->dateLong($log_key); ?>
+                                </a>
+								<?php $count++; ?>
+							<?php endforeach; ?>
                         </div>
-                        <div class="col-7 col-sm-9">
-                            <div class="tab-content" id="vert-tabs-tabContent">
-                                <?php $log_count = 1; ?>
-                                <?php foreach ($log_array as $log_key=>$log): ?>
-                                    <div class="tab-pane text-left fade <?php echo $log_count == 1 ? "show active":null; ?>" id="tab-<?php echo $log_key; ?>" role="tabpanel" aria-labelledby="<?php echo $log_key; ?>-tab">
-
-
-                                        <!-- The time line -->
-                                        <div class="timeline">
-                                            <!-- timeline time label -->
-                                            <div class="time-label">
-                                                <span class="bg-red"><?php echo $functions->dateLong($log_key); ?></span>
-                                            </div>
-                                            <!-- /.timeline-label -->
-                                            <?php $log_count = 1; ?>
-                                            <?php foreach ($log as $log_row): ?>
-                                                <!-- timeline item -->
-                                                <div>
-                                                    <i class="fas fa-clock bg-red"></i>
-                                                    <div class="timeline-item">
-                                                        <span class="time"><i class="fas fa-clock"></i> <?php echo $functions->date_long_and_time($log_row->log_datetime); ?></span>
-                                                        <h3 class="timeline-header"><b><?php echo $log_count; ?></b> - Bulunduğu Sayfa: <?php echo $log_row->log_key; ?></h3>
-
-                                                        <div class="timeline-body">
-                                                            Sayfa linki: <?php echo $log_row->log_page;  ?>
-                                                        </div>
+                    </div>
+                    <div class="col-7 col-sm-9">
+                        <div class="tab-content" id="vert-tabs-tabContent">
+							<?php $logCount = 1; ?>
+							<?php foreach ($data->logs as $log_key => $log): ?>
+                                <div class="tab-pane text-left fade <?php echo $logCount === 1 ? "show active" : null; ?>"
+                                     id="tab-<?php echo $log_key; ?>" role="tabpanel"
+                                     aria-labelledby="<?php echo $log_key; ?>-tab">
+                                    <div class="timeline">
+                                        <div class="time-label">
+                                            <span class="bg-red"><?php echo $functions->dateLong($log_key); ?></span>
+                                        </div>
+										<?php $itemCount = 1; ?>
+										<?php foreach ($log as $log_row): ?>
+                                            <div>
+                                                <i class="fas fa-clock bg-red"></i>
+                                                <div class="timeline-item">
+                                                        <span class="time">
+                                                            <i class="fas fa-clock"></i>
+                                                            <?php echo $functions->dateLongWithTime($log_row->log_datetime); ?>
+                                                        </span>
+                                                    <h3 class="timeline-header">
+                                                        <b><?php echo $itemCount; ?></b> - Bulunduğu
+                                                        Sayfa: <?php echo $log_row->log_key; ?>
+                                                    </h3>
+                                                    <div class="timeline-body">
+                                                        Sayfa linki: <?php echo $log_row->log_page; ?>
                                                     </div>
                                                 </div>
-                                                <!-- END timeline item -->
-                                                <?php $log_count++; ?>
-                                            <?php endforeach; ?>
-                                        </div>
-
-
+                                            </div>
+											<?php $itemCount++; ?>
+										<?php endforeach; ?>
                                     </div>
-                                    <?php $log_count++; ?>
-                                <?php endforeach; ?>
-                            </div>
+                                </div>
+								<?php $logCount++; ?>
+							<?php endforeach; ?>
                         </div>
-                    <?php else: ?>
-                        <div class="col-12">
-                            <div class="alert alert-info" role="alert">Kullanıcının her hangi bir haretketi bulunmuyor.</div>
+                    </div>
+				<?php else: ?>
+                    <div class="col-12">
+                        <div class="alert alert-info" role="alert">
+                            Kullanıcının her hangi bir haretketi bulunmuyor.
                         </div>
-                    <?php endif; ?>
-                </div>
+                    </div>
+				<?php endif; ?>
             </div>
-            <!-- /.card-body -->
-            <div class="card-footer">
-
-            </div>
-            <!-- /.card-footer-->
         </div>
-        <!-- /.card -->
-
-    </section>
-    <!-- /.content -->
-
-
-</div>
-<!-- /.content-wrapper -->
-
-<?php require $system->adminView('static/footer'); ?>
+    </div>
+</section>
