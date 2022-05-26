c518b39e0bb58b559049b69e9ec5ca7a7c165afa
mrokumus <okumusmr@gmail.com>
2022-05-26 11:42:28 +0300

    Update file-upload.php
    Yeni sistem düzeltmeleri

--- admin/Controller/ajax/file-upload.php
+++ admin/Controller/ajax/file-upload.php
@@ -1,25 +1,20 @@
 <?php
-/**
- * Author: Ozan SONAR
- * Mail : ozansonar1@gmail.com
- * User: Ozan PC
- * Date: 11.03.2021
- * Time: 15:54
- */
-if(isset($_FILES["file"]) && !empty($_FILES["file"]["name"])){
-    include_once($system->path("includes/System/FileUploader.php"));
-    $file = new \Includes\System\FileUploader($constants::fileTypePath);
-    $file->globalFileName = "file";
-    $file->uploadFolder = "mailing";
-    $file->maxFileSize = 5;
-    $uploaded = $file->fileUpload();
-    if($uploaded["result"] == 1){
-        $message["success"][] = "Resim başarıyla yüklendi.";
-        $message["img_path"] = $constants::fileTypePath["mailing"]["url"].$uploaded["img_name"];
-        $message["img_name"] = $uploaded["img_name"];
-    }else{
-        $message["reply"][] = $uploaded["result_message"];
-    }
-}else{
-    $message["reply"][] = "Lütfen bir dosya seçiniz.";
+
+use Mrt\MimozaCore\FileUploader;
+
+if (isset($_FILES["file"]) && !empty($_FILES["file"]["name"])) {
+	$file = new FileUploader($constants::fileTypePath);
+	$file->globalFileName = "file";
+	$file->uploadFolder = "mailing";
+	$file->maxFileSize = 5;
+	$uploaded = $file->fileUpload();
+	if ((int)$uploaded["result"] === 1) {
+		$message["success"][] = "Resim başarıyla yüklendi.";
+		$message["img_path"] = $constants::fileTypePath["mailing"]["url"] . $uploaded["img_name"];
+		$message["img_name"] = $uploaded["img_name"];
+	} else {
+		$message["reply"][] = $uploaded["result_message"];
+	}
+} else {
+	$message["reply"][] = "Lütfen bir dosya seçiniz.";
 }
